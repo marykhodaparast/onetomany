6e809a6b7ab33881aa27cc6a3e9387624c5c87a5
maryam khodaparast <khcomputergirl@gmail.com>
2020-09-14 12:22:28 +0430

    Relationships and mass assignment 14-93

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'body'
+    ];//we say laravel the fields are ok to create data in database = this is mass assignment
 }
